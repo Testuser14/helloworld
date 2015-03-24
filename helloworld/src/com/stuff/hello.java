cb231a667e722d4456eb62f796b4db8473b27cb1
Your Name <you@example.com>
2015-03-24 08:46:32 +0200

    Initial commit

---
+++ helloworld/src/com/stuff/hello.java
@@ -0,0 +1,14 @@
+package com.stuff;
+
+/**
+ * Created by nizamiislamovs on 15.23.3.
+ */
+public class hello {
+
+    //@Override
+    public static void main(String[] args)
+    {
+        System.out.println("dkfgjsdlkfjgslkfdj");
+        System.out.println("asdasdasdasdasdasdasdasdasdasdasd");
+    }
+}
